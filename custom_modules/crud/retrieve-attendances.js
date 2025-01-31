b0779abae9dd2ac3501cb203197697925e5d95e3
Francesco Dimuro <francesco.dimuro@adecco.it>
2025-01-31 23:42:17 +0100

    Refactor retrieveAttendances function to streamline file reading and improve code clarity

--- custom_modules/crud/retrieve-attendances.js
+++ custom_modules/crud/retrieve-attendances.js
@@ -3,9 +3,8 @@ import { readFile } from 'node:fs/promises';
 export default async function retrieveAttendances(fileName) {
     
     try {
-        let fileContent = await readFile(fileName, {encoding: 'utf-8'}, );
-        console.log(fileContent);
-        return fileContent;
+        // Read the content of the file and return it
+        return await readFile(fileName, {encoding: 'utf-8'}, );
     } catch (error) {
         console.error(error);
         return null;
