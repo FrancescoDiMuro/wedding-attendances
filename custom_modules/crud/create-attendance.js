fb803af45c77fa164d1ec5dfbb735d599fd6dea0
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2025-02-01 11:13:42 +0100

    Refactor createAttendance function to improve attendance data handling and ensure proper JSON structure

--- custom_modules/crud/create-attendance.js
+++ custom_modules/crud/create-attendance.js
@@ -7,29 +7,51 @@ export default async function createAttendance(request, fileName) {
     try {
 
         // Get the actual list of attendances
-        let attendances = await readFile(fileName, {encoding: 'utf-8', flag: 'a+'});
+        let attendancesFileContent = await readFile(fileName, {encoding: 'utf-8', flag: 'a+'});
+
+        // Set a new container for the JSON object
+        let attendancesObject = null;
 
         // If there is at least a record, 
         // then parse the content of the file as a JSON object
-        if(attendances.length > 0) {
-            attendances = JSON.parse(attendances);
+        if(attendancesFileContent.length > 0) {
+            attendancesObject = JSON.parse(attendancesFileContent);
         }
-        // Else, create an empty array
+        // Else, create an empty object
         else {
-            attendances = [];
+            attendancesObject = {attendances: []};
         }
         
+        // Get the request body
+        let attendanceData = await bodyParser(request);
+
+        // Get the data needed
+        let attendanceName = attendanceData.name;
+        let attendanceSurname = attendanceData.surname;
+
+        // Search the attendance by name and surname
+        let attendanceExists = attendancesObject.attendances.find((attendance) => {
+            return (attendance.name == attendanceName) && (attendance.surname == attendanceSurname);
+        }) != undefined;
+
+        // If the attendance already exists
+        if(attendanceExists) {
+            
+            // Inform the user
+            let text = `The attendance with name '${attendanceName}' and surname '${attendanceSurname}' already exists.`
+            
+            // Exit the function
+            return JSON.stringify({message: text});
+        }
+
         // Generate a random UUID for the attendance id
         let attendanceId = randomUUID();
-        
-        // Parse the request body
-        let attendanceData = await bodyParser(request);
         
         // Assign the attendance id to the attendance data
         attendanceData.id = attendanceId;
 
         // Push the newly created record in the attendences array/JSON object
-        attendances.push(attendanceData);
+        attendancesObject.attendances.push(attendanceData);
         
         // Stringify the data, with no replacer and four spaces
         let attendanceDataStringified = JSON.stringify(
@@ -41,7 +63,7 @@ export default async function createAttendance(request, fileName) {
         // Write to the file
         await writeFile(
             fileName, 
-            JSON.stringify(attendances), {
+            JSON.stringify(attendancesObject, null, 4), {
                 encoding: 'utf-8',
                 flag: 'w'
             }
